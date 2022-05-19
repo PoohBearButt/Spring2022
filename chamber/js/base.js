166b8fe9787aba7a3e66bbf3490b3b9bf0fc67bf
PoohBearButt <elisha.w716@gmail.com>
2022-05-18 23:45:39 -0600

    commit on 'main'

--- chamber/js/base.js
+++ chamber/js/base.js
@@ -5,7 +5,7 @@
 
 
 //this following code is from the lesson
-function toggleMenu(){
+function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 }
@@ -13,7 +13,15 @@ function toggleMenu(){
 const x = document.getElementById('hamburgerBtn');
 x.onclick = toggleMenu;
 
+let modifiedTime = new Date(document.lastModified);
+let timeDisplayed = `Last Updated: ${modifiedTime}`;
+document.querySelector("#lastUpdate").innerHTML = timeDisplayed;
+console.log(modifiedTime);
 
+
+//document.querySelector(
+//    "#lastModified"
+//   ).textContent = `Last Modification:${document.lastMotified}`;
 
 // function toggleMenu(){
 //     console.log("It worked")
